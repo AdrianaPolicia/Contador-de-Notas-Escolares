b3abf9b46ac9bef1670ee00eb5d58f6f20b2b41f
AdrianaPolicia <adriana.policia@sou.inteli.edu.br>
2025-02-10 11:53:11 -0300

    Add files via upload

---
+++ ContadordeNotas.js
@@ -0,0 +1,18 @@
+var Nota1 = 10
+var Nota2 = 7
+var Nota3 = 0 
+var media
+console.log(Nota1)
+console.log(Nota2)
+console.log(Nota3)
+console.log(media)
+
+media=(Nota1 + Nota2 + Nota3)/3
+
+if(media >= 7) {
+    console.log('aprovado')
+}
+
+if(media < 7) {
+    console.log('reprovado')
+}
